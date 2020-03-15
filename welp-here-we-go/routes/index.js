f0be01663dab7575b669d62c7c19e217bcce2f23
evansmike <evansmike@pm.me>
2020-03-15 15:28:34 -0400

    MVC overview, and adding controller for new view index.pug

--- welp-here-we-go/routes/index.js
+++ welp-here-we-go/routes/index.js
@@ -1,23 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  const mike = {name: 'Mike', age: 32, cool: true};
-  // res.send('Hey! It works!');
-  // res.json(mike);
-  // res.send(req.query.name);
-  // res.json(req.query);
-  res.render('hello', {
-    name: 'mike',
-    dog: 'Snickers'
-  });
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+// routes file will always just be a list of urls and then each action will be stored as a controller
+router.get('/', storeController.homePage);
 
 // See for more details https://expressjs.com/en/guide/routing.html
 
