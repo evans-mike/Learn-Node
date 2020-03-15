1b69fcc8ba25b45c60d666d0bd6a01a49e7989ee
evansmike <evansmike@pm.me>
2020-03-15 07:24:34 -0400

    forked from wesbos and changed variables.env, routes, and hello.pug

---
+++ welp-here-we-go/routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+// Do work here
+router.get('/', (req, res) => {
+  const mike = {name: 'Mike', age: 32, cool: true};
+  // res.send('Hey! It works!');
+  // res.json(mike);
+  // res.send(req.query.name);
+  // res.json(req.query);
+  res.render('hello', {
+    name: 'mike',
+    dog: "snickers"
+  });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+});
+
+// See for more details https://expressjs.com/en/guide/routing.html
+
+module.exports = router;
